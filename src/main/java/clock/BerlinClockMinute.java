9db8ad6a04b1d43dd3cc17d6d003cacd64d130e3
raval <ravalbb@gmail.com>
2017-10-06 12:03:46 +0200

    remove useless map implementation

--- src/main/java/clock/BerlinClockMinute.java
+++ src/main/java/clock/BerlinClockMinute.java
@@ -1,26 +1,13 @@
 package main.java.clock;
 
 
-import main.java.clock.position.AbstractBerlinClock;
 import main.java.clock.position.BerlinClockBottomMinute;
 import main.java.clock.position.BerlinClockTopMinute;
 import main.java.dto.BerlinClockTimeDTO;
-import main.java.enums.Position;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import static main.java.config.LampConfig.*;
-import static main.java.enums.Position.*;
 
 public final class BerlinClockMinute implements TimeConverter {
-    private Map<Position, AbstractBerlinClock> clockMap;
-
-    {
-        clockMap = new HashMap<>();
-        clockMap.put(TOP, new BerlinClockTopMinute());
-        clockMap.put(BOTTOM, new BerlinClockBottomMinute());
-    }
 
     public String convertTime(String textMinute) {
         Integer minutes = Integer.parseInt(textMinute);
@@ -29,14 +16,12 @@ public final class BerlinClockMinute implements TimeConverter {
     }
 
     private BerlinClockTimeDTO findPositionsForMinutes(Integer minutes) {
-        AbstractBerlinClock topPosition = clockMap.get(TOP);
-        AbstractBerlinClock bottomPosition = clockMap.get(BOTTOM);
-
         Integer topOnLamps = minutes / UNIT_PER_LAMP;
         Integer bottomOnLamps = minutes % UNIT_PER_LAMP;
 
-        String topHour = topPosition.switchOnLamps(topOnLamps, LAMPS_SIZE_FOR_MINUTES);
-        String bottomHour = bottomPosition.switchOnLamps(bottomOnLamps, LAMPS_SIZE);
+        String topHour = new BerlinClockTopMinute().switchOnLamps(topOnLamps, LAMPS_SIZE_FOR_MINUTES);
+        String bottomHour = new BerlinClockBottomMinute().switchOnLamps(bottomOnLamps, LAMPS_SIZE);
+
         return new BerlinClockTimeDTO(topHour, bottomHour);
     }
 
