c51ca2ae92d6e9f98c0e0f014cbcd282787c4ca9
rbajno <ravalbb@gmail.com>
2017-10-08 00:41:45 +0200

    move statement to method

--- src/main/java/clock/BerlinClockSecond.java
+++ src/main/java/clock/BerlinClockSecond.java
@@ -6,9 +6,13 @@ import static main.java.config.LampColorConfig.*;
 public final class BerlinClockSecond implements TimeConverter {
     public String convertTime(String textSeconds) {
         Integer seconds = Integer.parseInt(textSeconds);
-        if(seconds % 2 == 0) {
+        if(isYellowPosition(seconds)) {
             return switchOnYellowColor();
         }
         return switchOffColor();
+    }
+
+    private boolean isYellowPosition(Integer seconds) {
+        return seconds % 2 == 0;
     }
 }
